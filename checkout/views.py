68edb77ada38153698acd224fce07cd1cc877fe9
Ethan Trout <ethan-james-trout@outlook.com>
2024-10-18 17:19:52 +0000

    starting stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
-# Create your views here.
+import stripe
+
 
 def checkout(request):
-    bag = request.session.get('bag',{})
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request,"there's nothing in your bag")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency = settings.STRIPE_CURRENCY,
+
+    )
+    print(intent)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form':order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QBIdBA5pH2du6qUZ2oiLpbHZMl7Y9F1gx9WSlioIMMoSUnD5pS2soylRU2wBqWBFAp19iLkxLKQqGXSWGbXFwBS00SN1aDyd0',
+        'client_secret': 'test client secret',
     }
-    return render(request,template,context)
+
+    return render(request, template, context)
